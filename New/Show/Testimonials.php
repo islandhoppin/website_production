ea2eb278d81d6832267046bbf24d8c648a072d15
Islandhoppin <islandhoppincharters@gmail.com>
2019-07-28 20:30:59 +0000

    updated testimonials edit form

--- New/Show/Testimonials.php
+++ New/Show/Testimonials.php
@@ -174,10 +174,10 @@
 				    	</div>
 				    	<div>
 				    		<?php while( $row2 = $testimonials->fetch()) : ?>
-	                                        <div id="ex<?php echo $row2['testimonial_id'];?>" class="modal" style="background-color:#E5F5FA;">
-												<p><?php echo $row2['testimonial']; ?></p>
-												<a href="#" rel="modal:close" class="image featured" style="display: block; margin-left: auto; margin-right: auto; width: 50%;"><img src="images/logo.png" alt="" /></a>
-											</div>
+	                            <div id="ex<?php echo $row2['testimonial_id'];?>" class="modal" style="background-color:#E5F5FA;">
+									<p><?php echo $row2['testimonial']; ?></p>
+									<a href="#" rel="modal:close" class="image featured" style="display: block; margin-left: auto; margin-right: auto; width: 50%;"><img src="images/logo.png" alt="" /></a>
+								</div>
 							<?php endwhile ?>	
 				    	</div>
 					</div>
