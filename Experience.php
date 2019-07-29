06f2439a444f6981ea697dc4254653a914f23bd2
Islandhoppin <islandhoppincharters@gmail.com>
2019-07-29 01:59:54 +0000

    updated testimonials page

--- Experience.php
+++ Experience.php
@@ -586,7 +586,7 @@
 												</div>
 												<a href="#ex<?php echo $row5['testimonial_id'];?>" rel="modal:open" class="btn btn-lg btn-success"><subscript>Click to read the full review</subscript></a>
 											</div>
-											<div id="ex<?php echo $row5['testimonial_id'];?>" class="modal" style="background-color:#E5F5FA;">
+											<div id="ex<?php echo $row5['testimonial_id'];?>" class="modal" style="background-color:#E5F5FA; width: 50%;">
 												<p><?php echo $row5['testimonial']; ?></p>
 												<a href="#" rel="modal:close" class="image featured" style="display: block; margin-left: auto; margin-right: auto; width: 50%;"><img src="images/logo.png" alt="" /></a>
 											</div>
