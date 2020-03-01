466a4d90dd1a633ef6a3d060505af7bf9977c0f4
Brandon Mikelaitis <islandhoppincharters@gmail.com>
2020-03-01 01:01:19 +0000

    updated header photo

--- Book.php
+++ Book.php
@@ -33,7 +33,7 @@
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="assets/css/main_sub.css" />
 		<link rel="apple-touch-icon" href="images/favicon.png" />
 		<link rel="shortcut icon" href="images/favicon.png" />
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
