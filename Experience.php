22e037041dd346660bfe2fb6a58e8adbdc58a7cb
Brandon Mikelaitis <islandhoppincharters@gmail.com>
2021-02-02 17:56:17 +0000

    Testing paragraph spaces

--- Experience.php
+++ Experience.php
@@ -112,10 +112,7 @@
 		}
 		
 		.style-two {
-			border: 0;
-			height: 0;
-			border-top: 1px solid rgba(0, 0, 0, 0.1);
-			border-bottom: 1px solid rgba(255, 255, 255, 0.3);
+			margin-top: -50%;
 		}
 
 		</style>
